4e5be60a668fbe7bee13d0379f09d34355ff6ba0
sklit-dsk <danilasklovskii.dev@gmail.com>
2024-12-14 01:31:45 +0100

    UseState added to head

--- src/UseState.js
+++ src/UseState.js
@@ -1,9 +1,23 @@
+import { useState } from "react";
+
 function UseState() {
+
+    const [numbers, setNumbers] = useState([1, 2, 3, 4]);
+
+    const addNumber = () => {
+        const newNumber = Math.round(Math.random() * 10) + 1;
+        setNumbers([...numbers, newNumber]);
+    }
+
     return (
-        <div></div>
-        <div></div>
-        <div></div>
-        <button></button>
+        <div>
+            <ul>
+                {
+                numbers.map((num, index) => <li key={index}>{num}</li>)
+                }
+            </ul>
+            <button onClick={addNumber}>New number</button>
+        </div>
     )
 }
 
